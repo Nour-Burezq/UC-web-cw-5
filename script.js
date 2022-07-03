ace890bcffda0fc40e1b6befe46e6ced7396e82e
Nour-Burezq <107755709+Nour-Burezq@users.noreply.github.com>
2022-07-04 01:55:30 +0300

    part2

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ function BMI (weight = 60 ,height = 1.60){
 }
 console.log(BMI())
 
-
+function status (){
+    if (BMI()<18.5){
+        console.log("لديك نقص في الوزن");
+    } else if (BMI()>=18.5 && BMI()<25){
+        console.log("وزنك صحي")
+    }else if (BMI()>=25){
+        console.log ("لديك زيادة في الوزن")
+    }
+    }
