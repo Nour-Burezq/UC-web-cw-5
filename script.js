21ffc06a1e2f08a59cbf71fe1b2085403004d314
Nour-Burezq <107755709+Nour-Burezq@users.noreply.github.com>
2022-07-08 20:34:22 +0300

    PART3

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ function BMI (weight = 60 ,height = 1.60){
 }
 console.log(BMI())
 
-function status (){
-    if (BMI()<18.5){
-        console.log("لديك نقص في الوزن");
-    } else if (BMI()>=18.5 && BMI()<25){
-        console.log("وزنك صحي")
-    }else if (BMI()>=25){
-        console.log ("لديك زيادة في الوزن")
+function Status (num){
+    if (num<18.5){
+        return"لديك نقص في الوزن";
+    
+    } else if (num>=18.5 && num<25){
+        return"وزنك صحي";
+    }else if (num>=25){
+        return "لديك زيادة في الوزن";
+        
     }
     }
+
+function calculate (weight, height){
+        let bmi = BMI(weight, height);
+        let desc = Status(bmi);
+         document.getElementById("result").innerHTML=bmi;
+         document.getElementById("result").innerHTML+= desc;
+    }
+
+
